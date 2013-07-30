ec577120f54ef5c0e23e9f55c00254ea1c001bf2
Jan Berdajs <mrbrdo@gmail.com>
2013-07-30 15:13:48 +0200

    fix for rails 4 (https://github.com/rails/rails/commit/24c4195bab27ed9d76d0e126044e7f932de9e30e might be necessary for this to work properly)

--- lib/active_record_group_count/scope.rb
+++ lib/active_record_group_count/scope.rb
@@ -5,7 +5,12 @@ module ActiveRecordGroupCount
     module ExtensionMethods
       def count(*args)
         scope = except(:select).select("1")
-        query = "SELECT count(*) AS count_all FROM (#{scope.to_sql}) x"
+        scope_sql = if scope.klass.connection.respond_to?(:unprepared_statement)
+          scope.klass.connection.unprepared_statement { scope.to_sql }
+        else
+          scope.to_sql
+        end
+        query = "SELECT count(*) AS count_all FROM (#{scope_sql}) x"
         ActiveRecord::Base.connection.execute(query).first.try(:[], "count_all").to_i
       end
     end
